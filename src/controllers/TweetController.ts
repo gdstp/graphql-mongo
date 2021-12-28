d1ccdde191e73bb5112c81c003e2aeda5be102f7
gdstp <gabriel.teles.pinto@gmail.com>
2021-12-27 21:56:41 -0300

    feat: add findById method

--- src/controllers/TweetController.ts
+++ src/controllers/TweetController.ts
@@ -9,12 +9,51 @@ class TweetController {
     return await MongoTweet.find();
   }
 
+  @Query((returns) => Tweet, { name: 'tweet' })
+  async findById(@Arg('id') id: string) {
+    const tweet = await MongoTweet.findById(id);
+    if (!tweet) {
+      throw new Error('Tweet does not exists');
+    }
+    return tweet;
+  }
+
   @Mutation((returns) => Tweet, { name: 'createTweet' })
   async create(
     @Arg('author') author: string,
     @Arg('description') description: string
   ) {
     return await MongoTweet.create({ author, description, likes: 0 });
+  }
+
+  @Mutation((returns) => Tweet)
+  async upvoteTweet(@Arg('id') id: string) {
+    const tweet = await MongoTweet.findById(id);
+    if (!tweet) {
+      throw new Error('Tweet does not exists');
+    }
+
+    tweet.set({ likes: tweet.likes + 1 });
+
+    await tweet.save();
+    return tweet;
+  }
+
+  @Mutation((returns) => Tweet)
+  async downvoteTweet(@Arg('id') id: string) {
+    const tweet = await MongoTweet.findById(id);
+    if (!tweet) {
+      throw new Error('Tweet does not exists');
+    }
+
+    if (tweet.likes === 0) {
+      throw new Error('Cannot dislike this tweet');
+    }
+
+    tweet.set({ likes: tweet.likes - 1 });
+
+    await tweet.save();
+    return tweet;
   }
 }
 
